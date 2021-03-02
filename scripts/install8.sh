07312ab1a894f3ebb7f96a07d6aa4948fb948205
thuongtin <thuongtin@gmail.com>
2021-03-02 08:02:42 +0700

    Update install8.sh

--- scripts/install8.sh
+++ scripts/install8.sh
@@ -114,11 +114,8 @@ IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-echo "How many proxy do you want to create? Example 500"
-read COUNT
-
 FIRST_PORT=10000
-LAST_PORT=$(($FIRST_PORT + $COUNT))
+LAST_PORT=11000
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
